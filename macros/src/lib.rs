951aa5a438bebd03f7cebdf81a9cd9ef3e475af7
iori <shaorong0709@gmail.com>
2025-01-06 14:19:45 +0800

    derive macros for enum

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,2 +1,50 @@
+use proc_macro::TokenStream;
+use quote::quote;
+
+#[proc_macro_derive(EnumFrom)]
+pub fn derive_enum_from(input: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+    println!("{:#?}", input);
+
+    let ident = input.ident;
+
+    let generics = input.generics;
+
+    let variants = match input.data {
+        syn::Data::Enum(data) => data.variants,
+        _ => panic!("only support on enum"),
+    };
 
 
+
+    let from_impls = variants.iter().map(|variant| {
+        let var_ident = &variant.ident;
+
+        match &variant.fields {
+            syn::Fields::Unnamed(fields) => {
+                if fields.unnamed.len() != 1 {
+                    quote! {}
+                } else {
+                    let field = fields.unnamed.first().expect("should have one field");
+                    let ty = &field.ty;
+
+
+                    quote! {
+                        impl #generics From<#ty> for #ident #generics {
+                            fn from(value: #ty) -> Self {
+                                #ident::#var_ident(value)
+                            }
+                        }
+                    }
+                }
+            }
+            syn::Fields::Named(_fields) => quote! {},
+            syn::Fields::Unit => quote! {},
+        }
+    });
+
+    quote! {
+        #(#from_impls)*
+    }
+    .into()
+}
