e68b671c9f8aa48f4163bc397d15096bad02b223
iori <shaorong0709@gmail.com>
2025-01-09 11:23:33 +0800

    tracing and tracing-subscriber

---
+++ ecos-system/examples/axum_tracing.rs
@@ -0,0 +1,67 @@
+use std::time::Duration;
+
+use axum::{
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
+use tokio::{
+    net::TcpListener,
+    time::{sleep, Instant},
+};
+use tracing::{debug, info, instrument, level_filters::LevelFilter, warn};
+use tracing_subscriber::{
+    fmt::{self, format::FmtSpan},
+    layer::SubscriberExt,
+    util::SubscriberInitExt,
+    Layer,
+};
+
+const ADDR: &str = "0.0.0.0:8012";
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let file_appender = tracing_appender::rolling::daily("ecos-system/tmp/logs", "scosystem.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
+    let console = fmt::Layer::new()
+        .with_span_events(FmtSpan::NEW | FmtSpan::CLOSE)
+        .pretty()
+        .with_filter(LevelFilter::DEBUG);
+
+    let file = fmt::Layer::new()
+        .pretty()
+        .with_writer(non_blocking)
+        .with_filter(LevelFilter::INFO);
+
+    tracing_subscriber::registry()
+        .with(console)
+        .with(file)
+        .init();
+
+    let app = Router::new().route("/", get(index_handler));
+
+    info!("Starting server on {}", ADDR);
+    let listener = TcpListener::bind(ADDR).await?;
+
+    axum::serve(listener, app.into_make_service()).await?;
+    Ok(())
+}
+
+#[instrument]
+async fn index_handler() -> Response {
+    debug!("index handler started");
+    sleep(Duration::from_millis(11)).await;
+    let ret = long_task().await;
+    info!(http.status = 200, "index handler complteed");
+    ret
+}
+
+#[instrument]
+async fn long_task() -> Response {
+    let start = Instant::now();
+    sleep(Duration::from_millis(112)).await;
+    let elapsed = start.elapsed().as_millis() as u64;
+    warn!(app.task_duration = elapsed, "task takes too long");
+    "Hello, Rust!".into_response()
+}
--- ecos-system/examples/err.rs
+++ ecos-system/examples/err.rs
@@ -1,7 +1,24 @@
 use std::{fs, io::Read};
 
 use anyhow::Context;
-use ecos_system::MyError;
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum MyError {
+    #[error("I/O error: {0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("Parse error: {0}")]
+    Parse(#[from] std::num::ParseIntError),
+
+    #[error("Custom error: {0}")]
+    Custom(String),
+
+    #[error("Serialize json error: {0}")]
+    Serialize(#[from] serde_json::Error),
+}
+
 
 fn main() -> Result<(), anyhow::Error> {
     println!("size of MyError is {}", std::mem::size_of::<MyError>());
