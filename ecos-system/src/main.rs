f4d24db1a8a69dd5e8679513dbbd2363e0f94903
iori <shaorong0709@gmail.com>
2025-01-09 09:34:20 +0800

    study and use thiserror/anyhow

--- ecos-system/src/main.rs
+++ ecos-system/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs, io::Read};
+
+use anyhow::Context;
+use ecos_system::MyError;
+
+fn main() -> Result<(), anyhow::Error> {
+    println!("size of MyError is {}", std::mem::size_of::<MyError>());
+    let filename = "D:/settingsa.json";
+    let mut fd =
+        fs::File::open(filename).with_context(|| format!("Can not find file: {}", filename))?;
+    let mut str = String::new();
+    fd.read_to_string(&mut str)?;
+    println!("{:#?}", str);
+
+    fail_with_error()?;
+    Ok(())
+}
+
+fn fail_with_error() -> Result<(), MyError> {
+    Err(MyError::Custom("This is a custom error".to_string()))
 }
