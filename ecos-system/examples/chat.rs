1dc9da02e77f1346fe7968429c3edc7e612278b3
iori <shaorong0709@gmail.com>
2025-01-15 10:07:35 +0800

    broad chat

---
+++ ecos-system/examples/chat.rs
@@ -0,0 +1,166 @@
+use anyhow::Result;
+use dashmap::DashMap;
+use futures::{stream::SplitStream, SinkExt, StreamExt};
+use std::{fmt, net::SocketAddr, sync::Arc};
+use tokio::{
+    net::{TcpListener, TcpStream},
+    sync::mpsc,
+};
+use tokio_util::codec::{Framed, LinesCodec};
+use tracing::{info, level_filters::LevelFilter, warn};
+use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
+
+const MAX_MESSAGES: usize = 128;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let console = Layer::new().pretty().with_filter(LevelFilter::DEBUG);
+
+    tracing_subscriber::registry().with(console).init();
+    // console_subscriber::init();
+
+    let addr = "0.0.0.0:8080";
+    let listener = TcpListener::bind(addr).await?;
+    info!("Char serve start on {}", addr);
+
+    let state = Arc::new(State::default());
+
+    loop {
+        let (stream, addr) = listener.accept().await?;
+        info!("Accepted connection from {}", addr);
+        let state_cloned = Arc::clone(&state);
+        tokio::spawn(async move {
+            if let Err(e) = handle_client(state_cloned, addr, stream).await {
+                warn!("Failed to handle client {}: {}", addr, e);
+            }
+        });
+    }
+}
+
+async fn handle_client(state: Arc<State>, addr: SocketAddr, stream: TcpStream) -> Result<()> {
+    let mut stream = Framed::new(stream, LinesCodec::new());
+    stream.send("Enter your username:").await?;
+
+    let username = match stream.next().await {
+        Some(Ok(username)) => username,
+        Some(Err(e)) => return Err(e.into()),
+        None => return Ok(()),
+    };
+
+    let mut peer = state.add(addr, username, stream).await;
+
+    let message = Arc::new(Message::user_joined(&peer.username));
+
+    info!("{}", message);
+    state.broadcast(addr, message).await;
+
+    while let Some(line) = peer.stream.next().await {
+        let line = match line {
+            Ok(line) => line,
+            Err(e) => {
+                warn!("Failed to read line from {}: {}", addr, e);
+                break;
+            }
+        };
+
+        let message = Arc::new(Message::chat(&peer.username, line));
+
+        state.broadcast(addr, message).await;
+    }
+
+    state.peers.remove(&addr);
+
+    let message = Arc::new(Message::user_left(&peer.username));
+
+    info!("{}", message);
+    state.broadcast(addr, message).await;
+
+    Ok(())
+}
+
+#[derive(Debug, Default, Clone)]
+struct State {
+    peers: DashMap<SocketAddr, mpsc::Sender<Arc<Message>>>,
+}
+
+impl State {
+    async fn broadcast(&self, addr: SocketAddr, message: Arc<Message>) {
+        for peer in self.peers.iter() {
+            if peer.key() == &addr {
+                continue;
+            }
+            if let Err(e) = peer.value().send(Arc::clone(&message)).await {
+                warn!("Failed to send message to {}: {}", peer.key(), e);
+                self.peers.remove(peer.key());
+            }
+        }
+    }
+
+    async fn add(
+        &self,
+        addr: SocketAddr,
+        username: String,
+        stream: Framed<TcpStream, LinesCodec>,
+    ) -> Peer {
+        let (tx, mut rx) = mpsc::channel(MAX_MESSAGES);
+        self.peers.insert(addr, tx);
+
+        let (mut stream_sender, stream_receiver) = stream.split();
+
+        tokio::spawn(async move {
+            while let Some(message) = rx.recv().await {
+                if let Err(e) = stream_sender.send(message.to_string()).await {
+                    warn!("Failed to send message to {}: {}", addr, e);
+                    break;
+                }
+            }
+        });
+
+        Peer {
+            username,
+            stream: stream_receiver,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Peer {
+    username: String,
+    stream: SplitStream<Framed<TcpStream, LinesCodec>>,
+}
+
+#[derive(Debug)]
+enum Message {
+    UserJoined(String),
+    UserLeft(String),
+    Chat { sender: String, content: String },
+}
+
+impl Message {
+    fn user_joined(username: &str) -> Self {
+        let content = format!("{} has joined the chat", username);
+        Self::UserJoined(content)
+    }
+
+    fn user_left(username: &str) -> Self {
+        let content = format!("{} has left the chat", username);
+        Self::UserLeft(content)
+    }
+
+    fn chat(sender: impl Into<String>, content: impl Into<String>) -> Self {
+        Self::Chat {
+            sender: sender.into(),
+            content: content.into(),
+        }
+    }
+}
+
+impl fmt::Display for Message {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::UserJoined(content) => write!(f, "[{}]", content),
+            Self::UserLeft(content) => write!(f, "[{} :(]", content),
+            Self::Chat { sender, content } => write!(f, "{}: {}", sender, content),
+        }
+    }
+}
