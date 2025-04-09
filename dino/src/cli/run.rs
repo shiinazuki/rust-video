4d08552e5904e62fd07b3ac25b598d05931a458a
iori <shaorong0709@gmail.com>
2025-04-09 14:40:57 +0800

    use IntoJs/From/js for rust/js

--- dino/src/cli/run.rs
+++ dino/src/cli/run.rs
@@ -1,8 +1,8 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 use clap::Parser;
 
-use crate::{build_project, CmdExector, JsWorker};
+use crate::{build_project, CmdExector, JsWorker, Req};
 
 #[derive(Debug, Parser)]
 pub struct RunOpts {}
@@ -12,9 +12,16 @@ impl CmdExector for RunOpts {
         let filename = build_project(".")?;
         let context = fs::read_to_string(filename)?;
         let worker = JsWorker::try_new(&context)?;
-        
-        worker.run("await handlers.hello()")?;
-        
+
+        let req = Req::builder()
+            .method("GET")
+            .url("https://example.com")
+            .headers(HashMap::new())
+            .build();
+
+        let ret = worker.run("hello", req)?;
+        println!("Response {:#?}", ret);
+
         Ok(())
     }
 }
--- dino/src/engine.rs
+++ dino/src/engine.rs
@@ -1,5 +1,8 @@
+use std::collections::HashMap;
+
 use anyhow::Result;
-use rquickjs::{Context, Function, Object, Runtime};
+use rquickjs::{Context, FromJs, Function, IntoJs, Object, Promise, Runtime};
+use typed_builder::TypedBuilder;
 
 #[allow(unused)]
 pub struct JsWorker {
@@ -28,13 +31,64 @@ impl JsWorker {
         Ok(Self { rt, ctx })
     }
 
-    pub fn run(&self, code: &str) -> anyhow::Result<()> {
+    pub fn run(&self, name: &str, req: Req) -> anyhow::Result<Res> {
         self.ctx.with(|ctx| {
-            ctx.eval_promise(code)?.finish::<()>()?;
-            Ok::<_, anyhow::Error>(())
-        })?;
+            let global = ctx.globals();
+            let handlers: Object = global.get("handlers")?;
+            let fun: Function = handlers.get(name)?;
+            let v: Promise = fun.call((req,))?;
 
-        Ok(())
+            Ok::<_, anyhow::Error>(v.finish()?)
+        })
+    }
+}
+
+#[derive(Debug, TypedBuilder)]
+pub struct Req {
+    pub headers: HashMap<String, String>,
+
+    #[builder(setter(into))]
+    pub method: String,
+
+    #[builder(setter(into))]
+    pub url: String,
+
+    #[builder(default, setter(strip_option))]
+    pub body: Option<String>,
+}
+
+impl<'js> IntoJs<'js> for Req {
+    fn into_js(self, ctx: &rquickjs::Ctx<'js>) -> rquickjs::Result<rquickjs::Value<'js>> {
+        let obj = Object::new(ctx.clone())?;
+        obj.set("headers", self.headers)?;
+        obj.set("method", self.method)?;
+        obj.set("url", self.url)?;
+        obj.set("body", self.body)?;
+
+        Ok(obj.into())
+    }
+}
+
+#[derive(Debug)]
+pub struct Res {
+    pub status: u16,
+    pub headers: HashMap<String, String>,
+    pub body: Option<String>,
+}
+
+impl<'js> FromJs<'js> for Res {
+    fn from_js(_ctx: &rquickjs::Ctx<'js>, value: rquickjs::Value<'js>) -> rquickjs::Result<Self> {
+        let obj = value.into_object().unwrap();
+
+        let status: u16 = obj.get("status")?;
+        let headers: HashMap<String, String> = obj.get("headers")?;
+        let body: Option<String> = obj.get("body")?;
+
+        Ok(Res {
+            status,
+            headers,
+            body,
+        })
     }
 }
 
@@ -44,8 +98,30 @@ mod tests {
 
     #[test]
     fn js_worker_should_run() {
-        let code = r#"(function(){async function hello(){print("hello world");return"hello";}return{hello:hello};})();"#;
+        let code = r#"
+            (function(){
+                async function hello(req){
+                    return {
+                        status: 200,
+                        headers: {
+                            "content-type":"application.json"
+                            },
+                            body: JSON.stringify(req),
+                        };
+                    }
+                    return{
+                        hello:hello
+                    };
+            })();
+        "#;
+        let req = Req::builder()
+            .method("GET")
+            .url("https://example.com")
+            .headers(HashMap::new())
+            .build();
+
         let worker = JsWorker::try_new(code).unwrap();
-        worker.run("await handlers.hello()").unwrap();
+        let ret = worker.run("hello", req).unwrap();
+        assert_eq!(ret.status, 200);
     }
 }
