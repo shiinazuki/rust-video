37897ff13b1a9f3c57ddf8a46c1c83b7434403fd
iori <shaorong0709@gmail.com>
2025-04-15 10:22:59 +0800

    support algo js binding

--- napi-example/index.js
+++ napi-example/index.js
@@ -310,6 +310,7 @@ if (!nativeBinding) {
   throw new Error(`Failed to load native binding`)
 }
 
-const { plus } = nativeBinding
+const { JsAlgo, plus } = nativeBinding
 
+module.exports.JsAlgo = JsAlgo
 module.exports.plus = plus
